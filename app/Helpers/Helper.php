9a67e81588c94f6446401edabf9de85e79b88773
Your Name <you@example.com>
2018-12-30 20:41:40 +0530

    sidebar change

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -143,4 +143,6 @@ class Helper {
         return $filename;
     }
 
+    dd($post);
+
 }
--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -7,65 +7,98 @@
             <img src="http://jskrishna.com/work/merkury/images/circle-logo.png" alt="merkery_logo" class="visible-xs visible-sm circle-logo"> -->
         </a>
     </div>
-    <div class="navi nav-side-menu">
-        <ul>
-            <li class="active">
-                <a href="{{ url('auth/admin') }}">
-                    <i class="fa fa-home" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Dashboard</span>
-                </a>
+ 
+    <nav class="navbar navbar-inverse sidebar" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-sidebar-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Brand</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="{{ url('auth/admin') }}">Dashbaord<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+                <li ><a href="{{ url('auth/admin/profile') }}">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+                <li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Users <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+                    <ul class="dropdown-menu forAnimate" role="menu">                        
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/users') }}">All Users</a></li>
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/adduser') }}">Add User</a></li>
+                    </ul>
                 </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-tasks" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Workflow</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-bar-chart" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Statistics</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-calendar" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Calender</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ url('auth/admin/users') }}">
-                    <i class="fa fa-group" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Users</span>
-                </a>
-            </li>
-            <li>
-                <a href="{{ url('auth/admin/adduser') }}">
-                    <i class="fa fa-user" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Add User</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class="fa fa-cog" aria-hidden="true"></i>
-                    <span class="hidden-xs hidden-sm">Setting</span>
-                </a>
-            </li>
-            <li  data-toggle="collapse" data-target="#products" class="collapsed active">
-                  <a href="#"><i class="fa fa-gift fa-lg"></i> UI Elements <span class="arrow"></span></a>
+
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Posts <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+                    <ul class="dropdown-menu forAnimate" role="menu">                        
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/posts') }}">All Posts</a></li>
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/add/post') }}">Add Post</a></li>
+                    </ul>
                 </li>
-                <ul class="sub-menu collapse" id="products">
-                    <li class="active"><a href="#">CSS3 Animation</a></li>
-                    <li><a href="#">General</a></li>
-                    <li><a href="#">Buttons</a></li>
-                    <li><a href="#">Tabs & Accordions</a></li>
-                    <li><a href="#">Typography</a></li>
-                    <li><a href="#">FontAwesome</a></li>
-                    <li><a href="#">Slider</a></li>
-                    <li><a href="#">Panels</a></li>
-                    <li><a href="#">Widgets</a></li>
-                    <li><a href="#">Bootstrap Model</a></li>
-                </ul>
-        </ul>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+                    <ul class="dropdown-menu forAnimate" role="menu">                        
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/posts') }}">All Pages</a></li>
+                        <li class="divider"></li>
+                        <li><a href="{{ url('auth/admin/add/page') }}">Add Page</a></li>
+                    </ul>
+                </li>
+               <!--  <li><a href="#">Home<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-home"></span></a></li>
+                <li ><a href="#">Profile<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-user"></span></a></li>
+                <li ><a href="#">Messages<span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-envelope"></span></a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Settings <span class="caret"></span><span style="font-size:16px;" class="pull-right hidden-xs showopacity glyphicon glyphicon-cog"></span></a>
+                    <ul class="dropdown-menu forAnimate" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li> -->
+            </ul>
+        </div>
     </div>
+</nav>
 </div>
+
+<script type="text/javascript">
+        function htmlbodyHeightUpdate(){
+        var height3 = $( window ).height()
+        var height1 = $('.nav').height()+50
+        height2 = $('.main').height()
+        if(height2 > height3){
+            $('html').height(Math.max(height1,height3,height2)+10);
+            $('body').height(Math.max(height1,height3,height2)+10);
+        }
+        else
+        {
+            $('html').height(Math.max(height1,height3,height2));
+            $('body').height(Math.max(height1,height3,height2));
+        }
+        
+    }
+    $(document).ready(function () {
+        htmlbodyHeightUpdate()
+        $( window ).resize(function() {
+            htmlbodyHeightUpdate()
+        });
+        $( window ).scroll(function() {
+            height2 = $('.main').height()
+            htmlbodyHeightUpdate()
+        });
+    });
+</script>
